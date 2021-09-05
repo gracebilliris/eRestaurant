e331692649ac16069e4be26e11e155b7788f86f5
13888767 <13888767@student.uts.edu.au>
2021-09-05 21:53:15 +1000

    User can create booking

--- app/controllers/booking.controller.js
+++ app/controllers/booking.controller.js
@@ -1,27 +1,29 @@
 const db = require("../models");
-const Booking = require("../models/booking.model");
-const User = require("../models/user.model");
+const Booking = db.booking;
 
+//Create and Save Booking
 exports.create = (req,res) => {
-    if(!req.body.date) {
-      res.status(500).send({ message: "Content can not be empty!" });
-      return;
-    }
+  //Validate request
+  if(!req.body.date || !req.body.time || !req.body.seats) {
+    res.status(500).send({message: "Content can not be empty"});
+    return;
+  }
 
-    //Create
-    const booking = new Booking({
-      username: req.body.username,
-      date: req.body.date,
-      time: req.body.time,
-      seats: req.body.seats,
+  //Create Booking
+  const booking = new Booking({
+    username: req.body.username,
+    date: req.body.date,
+    time: req.body.time,
+    seats: req.body.seats
+  });
+
+  //Save in Database
+  booking
+    .save(booking)
+    .then(data => {
+      res.status(500).send({message: "Booking Made"});
+    })
+    .catch(err => {
+      res.status(500).send({message: "Error occur while making booking"});
     });
-
-    booking
-      .save(booking)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({ message: "Something happen i dont know u figure it out"});
-      })
-}
+};
--- app/models/booking.model.js
+++ app/models/booking.model.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
-
-const Booking = mongoose.model(
-  "Booking",
-  new mongoose.Schema({
-    username: String,
-    date: String,
-    time: String,
-    seats: String
-  })
-);
-
-module.exports = Booking;
+module.exports = mongoose => {
+  const Booking = mongoose.model(
+    "booking",
+    mongoose.Schema(
+      {
+        username: String,
+        date: String,
+        time: String,
+        seats: String
+      },
+    )
+  );
+  return Booking;
+}
