7da9df2eced303e05a26a3b8c55247a26b8dfd89
13888767 <13888767@student.uts.edu.au>
2021-10-02 14:46:54 +1000

    add the option to pick different location

--- src/components/financialReport.js
+++ src/components/financialReport.js
@@ -9,6 +9,7 @@ class FinancialReport extends React.Component {
     this.retrieveDates = this.retrieveDates.bind(this);
     this.refreshList = this.refreshList.bind(this);
     this.setActiveDate = this.setActiveDate.bind(this);
+    this.onChangeStatus = this.onChangeStatus.bind(this);
 
     //Define each attribute
     this.state = {
@@ -17,11 +18,18 @@ class FinancialReport extends React.Component {
       currentIndex: -1,
       menus: [],
       totalcost: "",
+      status: false
     };
   }
 
   componentDidMount() {
     this.retrieveDates();
+  }
+
+  onChangeStatus(e) {
+   this.setState({
+     status: e.target.value
+   })
   }
 
   retrieveDates() {
@@ -65,45 +73,57 @@ class FinancialReport extends React.Component {
       <div style={{fontFamily: "Times New Roman", textAlign: "center", "width":"80%", "marginLeft": "130px"}}>
         <hr className="new5"></hr>
         <h3>Financial Report</h3>
-        <Grid container>
-          <Grid item md={4}>
-            <h2>Dates List</h2>
-            <div className="list-group">
-              {dates && dates.map((date, index) => (
-                <ListItem selected={index === currentIndex} onClick={() => this.setActiveDate(date, index)} divider button style={{padding: "20px"}} key={index}> {"Date: " + date} </ListItem>
-              ))}
-            </div>
-          </Grid>
-          <Grid item md={8}>
-            {currentDate ? (
-              <div className="beige-border">
-                <br/>
-                <h5>Menu Items:</h5>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Item</th>
-                        <th>Quantity</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentDate.meals.map((meal, index) => (
+        <div>
+          <label htmlFor="username">Select Location: </label>
+          <select style={{ marginLeft: "5px" }}
+            onChange={this.onChangeStatus}>
+            <option selected value={false} />
+            <option value={true}>North Sydney</option>
+          </select>
+        </div>
+        {this.state.status ? (
+          <div>
+          <Grid container>
+            <Grid item md={4}>
+              <h2>Dates List</h2>
+              <div className="list-group">
+                {dates && dates.map((date, index) => (
+                  <ListItem selected={index === currentIndex} onClick={() => this.setActiveDate(date, index)} divider button style={{padding: "20px"}} key={index}> {"Date: " + date} </ListItem>
+                ))}
+              </div>
+            </Grid>
+            <Grid item md={8}>
+              {currentDate ? (
+                <div className="beige-border">
+                  <br/>
+                  <h5>Menu Items:</h5>
+                    <table>
+                      <thead>
                         <tr>
-                          <td>{meal.name}</td>
-                          <td>{meal.quantity}</td>
+                          <th>Item</th>
+                          <th>Quantity</th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                <br/>
-                  <label><strong>Total Cost:</strong></label>{" $"}{currentDate.totalcost}
-              </div>
-             ) : (
-              <div style={{display: "block", paddingTop: "75px", paddingBottom: "75px", marginLeft:"100px"}}>
-              </div>
-            )}
+                      </thead>
+                      <tbody>
+                        {currentDate.meals.map((meal, index) => (
+                          <tr>
+                            <td>{meal.name}</td>
+                            <td>{meal.quantity}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  <br/>
+                    <label><strong>Total Cost:</strong></label>{" $"}{currentDate.totalcost}
+                </div>
+               ) : (
+                <div style={{display: "block", paddingTop: "75px", paddingBottom: "75px", marginLeft:"100px"}}>
+                </div>
+              )}
+            </Grid>
           </Grid>
-        </Grid>
+          </div>
+        ) : (<div>Please Pick Location</div>)}
         <hr className="new5"></hr>
       </div>
     );
