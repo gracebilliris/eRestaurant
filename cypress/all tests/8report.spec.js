371ed3f25e30653544f6680d2d103ec8bc1df8c5
G Billiris <10949109+gracebilliris@users.noreply.github.com>
2021-10-05 14:57:51 +1100

    Finalising all tests

---
+++ cypress/all tests/8report.spec.js
@@ -0,0 +1,28 @@
+describe('Financial Report Function', () => {
+    it('Going to Login Up Page', () => {
+        cy.visit('/')
+        cy.findByText('Login').click()
+    })
+
+    it('Login Customer', () => {
+        cy.wait(3000)
+        cy.findByRole('textbox', { name: 'username'}).click().type('manager')
+        cy.findByRole('textbox', { name: 'password'}).click().type('manager')
+        cy.findByRole('button', { name: 'Login'}).click()
+    })
+
+    it('Open Report page', () => {
+        cy.wait(3000)
+        cy.findByText('Report').click()
+    })
+
+    it('Pick Location', () => {
+        cy.wait(3000)
+        cy.get('select').select('true')
+    })
+
+    it('Pick 2021-11-21', () => {
+        cy.wait(3000)
+        cy.findByText('Date: 2021-11-21').click()
+    })
+})
