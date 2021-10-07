ae7369b792a96ff9c8cd42f2cbef2d00fd262c06
13888767 <13888767@student.uts.edu.au>
2021-10-08 08:28:14 +1100

    format doccument

--- src/components/staffEditDetails.js
+++ src/components/staffEditDetails.js
@@ -5,21 +5,21 @@ import { Link, Switch, Route } from "react-router-dom";
 import staffDetails from "../components/staffDetails";
 
 class EditStaffDetails extends Component {
-  constructor(props) {
-    super(props);
-      this.onChangeEmail = this.onChangeEmail.bind(this);
-      this.getStaff = this.getStaff.bind(this);
-      this.updateStaff = this.updateStaff.bind(this);
+    constructor(props) {
+        super(props);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.getStaff = this.getStaff.bind(this);
+        this.updateStaff = this.updateStaff.bind(this);
 
-      this.state = {
-        currentStaff: {
-            id: null,
-            username: "",
-            role: "",
-            email: ""
-        },
-        message: ""
-      };
+        this.state = {
+            currentStaff: {
+                id: null,
+                username: "",
+                role: "",
+                email: ""
+            },
+            message: ""
+        };
     }
 
     componentDidMount() {
@@ -42,30 +42,32 @@ class EditStaffDetails extends Component {
 
     getStaff(staffId) {
         StaffDataService.getStaff(staffId)
-        .then(response => {
-            this.setState({
-                currentStaff: response.data
-            });
-            console.log(response.data);
-        })
-        .catch(e => {
-            console.log(e);
-        });
+            .then(response => {
+                this.setState({
+                    currentStaff: response.data
+                });
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            }
+        );
     }
 
     updateStaff() {
         StaffDataService.updateStaff(
             this.state.currentStaff
         )
-        .then(response => {
-            console.log(response.data);
-            this.setState({
-                message: "Staff Details were updated successfully!"
-            });
-        })
-        .catch(e => {
-            console.log(e);
-        });
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    message: "Staff Details were updated successfully!"
+                });
+            })
+            .catch(e => {
+                console.log(e);
+            }
+        );
     }
 
     render() {
@@ -75,45 +77,45 @@ class EditStaffDetails extends Component {
             <div>
                 <hr className="new5" ></hr>
                 {currentStaff ? (
-                <div style={{fontFamily: "Times New Roman", textAlign: "center"}}>
-                <h3>Staff List</h3>
-                <form style={{transform: "translateY(-10%)"}}>
-                    <div>
-                        <label htmlFor="username">Staff Name</label>
-                        <TextField type="text" className="form-control" name="username" value={currentStaff.username} disabled/>
+                    <div style={{ fontFamily: "Times New Roman", textAlign: "center" }}>
+                        <h3>Staff List</h3>
+                        <form style={{ transform: "translateY(-10%)" }}>
+                            <div>
+                                <label htmlFor="username">Staff Name</label>
+                                <TextField type="text" className="form-control" name="username" value={currentStaff.username} disabled />
+                            </div>
+                            <div>
+                                <label htmlFor="text">Role</label>
+                                <TextField type="text" className="form-control" name="role" value={"Staff"} disabled />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <TextField type="email" className="form-control" name="email" value={currentStaff.email} onChange={this.onChangeEmail} required />
+                            </div>
+
+                            <div className="form-group" style={{ display: "inline-flex" }}>
+                                <div>
+                                    <Button onClick={this.deleteStaff}> Delete</Button>
+                                </div>
+                            </div>
+                            <br />
+                            <div style={{ display: 'inline-block' }}>
+                                <Link style={{ WebkitTextFillColor: "black" }} to={"/staffdetails"}>Go Back?</Link>
+                                <Switch>
+                                    <Route exact path={"/staffdetails"} component={staffDetails} />
+                                </Switch>
+                            </div>
+                        </form>
+                        <p>{this.state.message}</p>
                     </div>
-                    <div>
-                        <label htmlFor="text">Role</label>
-                        <TextField type="text" className="form-control" name="role" value={"Staff"} disabled/>
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <TextField type="email" className="form-control" name="email" value={currentStaff.email} onChange={this.onChangeEmail} required/>
-                    </div>
-                    
-                    <div className="form-group" style ={{display: "inline-flex"}}>
-                       <div>
-                            <Button onClick={this.deleteStaff}> Delete</Button>
-                        </div>
-                    </div>
-                    <br/>
-                    <div style={{display: 'inline-block'}}>
-                        <Link style={{WebkitTextFillColor: "black"}} to={"/staffdetails"}>Go Back?</Link>
-                        <Switch>
-                            <Route exact path={"/staffdetails"} component={staffDetails}/>
-                        </Switch>
-                    </div>
-                </form>
-                <p>{this.state.message}</p>
-                </div>
                 ) : (
-                <div>
-                    <br />
-                    <p>Please click on a Staff...</p>
-                </div>
-            )}
-            <hr className="new5" ></hr>
-        </div>
+                    <div>
+                        <br />
+                        <p>Please click on a Staff...</p>
+                    </div>
+                )}
+                <hr className="new5" ></hr>
+            </div>
         );
     }
 }
